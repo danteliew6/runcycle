d58decd61979970685f58835596acf856fddceae
danteliew6 <danteliew6@gmail.com>
2020-11-09 00:27:00 +0800

    Update home.js

--- runcycle/home.js
+++ runcycle/home.js
@@ -8,9 +8,11 @@ function getAllEvents() {
 
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            // console.log(this.responseText);
             const data = JSON.parse(this.responseText);
-            let upcomingEvents = document.getElementById('upcomingEvents');
-            upcomingEvents.innerHTML = "";
+            console.log(data);
+            let myEvents = document.getElementById('myEvents');
+            myEvents.innerHTML = "";
 
             for (events of data.records) {
                 if (events.activity == "Run") {
@@ -20,27 +22,42 @@ function getAllEvents() {
                     var image = "cycle.png";
                 }
 
+                if (events.participants >= events.capacity) {
+                    var disabled = `disabled`;
+                    var button = `btn btn-secondary`;
+                    var buttonMsg = `Event Full`;
+                }
+                else {
+                    var disabled = ``; 
+                    var button = `btn btn-success`;
+                    var buttonMsg = `Join Event`;
+                }
 
                 // console.log(events);
-                upcomingEvents.innerHTML += 
+                myEvents.innerHTML += 
                 ` <div class="col-lg-3 col-md-6 m-4">
                     <div class="card h-100 border-1 shadow" style="width: 18rem;">
                       <img class="card-img-top" src="${image}" style = "background-color:grey;" alt="Image cannot be displayed">
                     <div class="card-body text-left">
                       <h4 class="card-title">${events.title}</h4> 
                       <p class="card-text">
-                        Start Point: ${events.start_point} <br>
-                        End Point: ${events.end_point}
+                        Start Point: <br><b>${events.start_point}</b> <br><br>
+                        End Point: <br><b>${events.end_point}</b><br><br>
+                        Date and Time:<br><b>${events.event_datetime}</b>
                       </p>
-                      
-                      <span><b>Details:</b></span><br>
-                      <p>${events.event_datetime}<br>Duration: ${events.duration} hours
-                          <br>Capacity: ${events.participants}/${events.capacity}</p> 
-                          <br><b>Created by: ${events.username}</b>             
                     </div>
-                    <div class="card-footer text-center p-4">
+                    <div class="card-body text-left">
+
+                    </div>
+                    <div class="card-footer text-center p-3"> 
+                        <div class = "text-left">
+                            <small>
+                            Capacity: ${events.participants}/${events.capacity}<br>
+                            <b>Created by: ${events.username}</b>
+                            </small><br><br>  
+                        </div>
                       <a href="event.html?event_id=${events.event_id}" class="btn btn-dark mr-2">Details</a>
-                      <button type = "button" class="btn btn-success" id = "event${events.event_id}" onclick = "updateJoin(${events.event_id})">Join Event</a>
+                      <button type = "button" class="${button}" id = "event${events.event_id}" onclick = "checkHost(${events.event_id})" ${disabled}>${buttonMsg}</button>
                     </div>
                     </div>
                 </div>`;
@@ -65,6 +82,7 @@ function checkJoined(event_id) {
             let eventToUpdate = document.getElementById(`event${event_id}`);
             eventToUpdate.setAttribute('class', 'btn btn-danger');
             eventToUpdate.innerHTML = "Cancel";
+            eventToUpdate.removeAttribute(`disabled`);
 
         }
     }
@@ -74,61 +92,84 @@ function checkJoined(event_id) {
 }
 
 
-
-function updateJoin(event_id) {
-    // console.log(event_id);
-    eventToUpdate = document.getElementById(`event${event_id}`);
-    checkHost(event_id);
-
-    if (eventToUpdate.getAttribute('class') == "btn btn-success") {
-        const url = `functions/joinEvent.php?event_id=${event_id}`;
-        const request = new XMLHttpRequest();
+function checkHost(event_id) {
+    const url = `functions/checkHost.php?event_id=${event_id}`;
+    const request = new XMLHttpRequest();
     
+    var triggered = 0
     
-        request.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                alert('Event joined succesfully! Have a good workout!')
-                eventToUpdate.setAttribute('class', "btn btn-danger");
-                eventToUpdate.innerHTML = "Cancel";         
-            }
+    request.onreadystatechange = function() {
+        console.log(triggered);
+        if (triggered < 1 && this.readyState == 4 && this.status == 200) {           
+            triggered++;     
+            return cancelEvent(true, event_id);
+            // request.abort();
         }
-        request.open("GET", `${url}`, true);
-        request.send();
+        else if (triggered < 1 && this.readyState == 4 && this.status == 404) {
+            triggered++
+            return cancelEvent(false, event_id);
+            // request.abort();
+        }
+        
     }
-    else {
+
+ 
+    request.open('GET', `${url}`, true);
+    request.send();
+    
+}
+
+
+function cancelEvent(isHost, event_id) {
+    eventToUpdate = document.getElementById(`event${event_id}`); 
+    if (isHost) {
+        if (confirm('You are the host. Are you sure you want to cancel this event?')) {
+            // eventToUpdate = document.getElementById(`event${event_id}`);
+            const url = `functions/cancelEvent.php?event_id=${event_id}`;
+            const request = new XMLHttpRequest();
+            request.open("GET", `${url}`, true);
+            request.send();
+
+            alert('Event removed!');
+            eventToUpdate.setAttribute('class', "btn btn-success");
+            eventToUpdate.innerHTML = "Join Event"; 
+            console.log('Event removed');
+            location.reload(); 
+        }
+    }
+    else if (eventToUpdate.innerHTML == "Cancel") {
         const url = `functions/removeParticipant.php?event_id=${event_id}`;
         const request = new XMLHttpRequest();
     
     
         request.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                alert('Event removed!');
                 eventToUpdate.setAttribute('class', "btn btn-success");
-                eventToUpdate.innerHTML = "Join Event";         
+                eventToUpdate.innerHTML = "Join Event"; 
+                alert('Event removed!');
+                location.reload();        
             }
         }
         request.open("GET", `${url}`, true);
         request.send();
-
     }
-}
-
-function checkHost(event_id) {
-    const url = `functions/getEvent.php?event_id=${event_id}`;
-    const request = new XMLHttpRequest();
-
-    request.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const data = JSON.parse(this.responseText);
-            // console.log(data.records[0].username);
-
-            let username = data.records[0].username;
-
-            
+    else {
+        const url = `functions/joinEvent.php?event_id=${event_id}`;
+        const request = new XMLHttpRequest();
+    
+    
+        request.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                eventToUpdate.setAttribute('class', "btn btn-danger");
+                eventToUpdate.innerHTML = "Cancel";
+                alert('Event joined succesfully! Have a good workout!');
+                location.reload();        
+            }
         }
+        request.open("GET", `${url}`, true);
+        request.send();
     }
-    request.open("GET", `${url}`, true);
-    request.send();
+
 }
 
 function getWeather(){
