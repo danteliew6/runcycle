ef1ed06201a5bcda2b4084fae66f7b1dbf269747
danteliew6 <danteliew6@gmail.com>
2020-11-09 17:17:16 +0800

    form validation for registration

--- runcycle/functions/newUser.php
+++ runcycle/functions/newUser.php
@@ -1,14 +1,25 @@
 <?php
     require_once "common.php";
     $dao = new UserDAO();
+    $isValidUsername = !empty($_POST['username']);
+    $isValidEmail = !empty($_POST['email']) && strpos($_POST['email'], '@') !== false;
+    $isValidPassword = !empty($_POST['password']) && $_POST['password'] == $_POST['confirm_password'];
 
-
-    if (isset($_POST['username']) && isset($_POST['email']) && $_POST['password'] == $_POST['confirm_password']) {
+    if ($isValidUsername && $isValidEmail && $isValidPassword) {
         $isAddOk = $dao -> newUser($_POST['username'], $_POST['password'], $_POST['email']);
         header("Location: ../login.html");
         exit();
     }
 
-    header("Location: ../register.html?error=true");
+    $errorMessage = '';
+    
+    $errorMessage .= !$isValidEmail ? 'email' : '';
+    $errorMessage .= !$isValidUsername ? 'username' : '';
+    $errorMessage .= !$isValidPassword ? 'password' : '';
+    
+    // echo $errorMessage;
+    header("Location: ../register.html?error=$errorMessage");
+    exit();
+
  
 ?>
